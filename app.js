c6a6a38b5a026f6428d197fcd9a75fbac9811eba
namkikim0718 <113903598+namkikim0718@users.noreply.github.com>
2023-03-16 10:04:26 +0900

    Update app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-const loginInput = document.querySelector(".login-form input");
-const loginButton = document.querySelector(".login-form button");
-
-function onLoginClick() {
-    alert("No!!");
-}
-
-loginButton.addEventListener("click", onLoginClick);
